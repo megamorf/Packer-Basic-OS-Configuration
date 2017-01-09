61f06a29ccf2cbfcafdf52fb582b65d09d7c4046
Administrator <xnem@outlook.com>
2017-01-10 00:09:37 +0100

    Add configuration script

---
+++ Scripts/Configuration.ps1
@@ -0,0 +1,19 @@
+﻿# Completely remove SMBv1 support
+# https://blogs.technet.microsoft.com/josebda/2015/04/21/the-deprecation-of-smb1-you-should-be-planning-to-get-rid-of-this-old-smb-dialect/
+Remove-WindowsFeature FS-SMB1
+
+
+
+# Disable server manager launch at logon
+New-ItemProperty -Path 'HKLM:\Software\Microsoft\ServerManager' -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value '0x1' -Force
+
+# Set shell to PowerShell instead of cmd 
+# launch sconfig at logon
+Set-ItemProperty -Path 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name Shell -Value 'powershell.exe -noexit -Command "Set-Location ${Env:USERPROFILE};start sconfig"'
+
+# Harden user enumeration via NetSession
+.\Common\NetCease\NetCease.ps1
+Restart-Service -Name LanmanServer -Force
+
+# Harden user enumeration via SAM-Remote (SAMR) protocol
+.\Server-2016\SAMRi10\SAMRi10.ps1
